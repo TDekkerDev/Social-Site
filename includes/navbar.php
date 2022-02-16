ec2b105536399659ed7b5245d0278964c69e877e
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-02-16 14:30:12 +0100

    ww

---
+++ includes/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/hellowordsource/Social-Site/index.php">social-site</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="/hellowordsource/Social-Site/user/user_Bericht_form.php">link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">lnik</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ user/user_Bericht_form.php
@@ -0,0 +1,27 @@
+<?php include "../includes/header.php"; ?>
+<?php include "../includes/navbar.php"; ?>
+<div class="container">
+<form class="col-6" action="#" method="post">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+           <h2>wat is de auteur</h2>
+           <input class="form-control" type="text" name="auteur" required>
+           <br>
+           <h2>wat is de title</h2>
+           <input class="form-control" type="text" name="titel" required>
+           <br>
+           <h2>wat is het bericht</h2>
+           <input class="form-control" type="text" name="bericht" required>
+           <h2>upload een foto</h2> 
+           <div class="input-group mb-3">
+           <input type="file"class="form-control" id="inputGroupFile02" name="image">
+           </div>
+           <br>
+           <input class="btn btn-primary"; type="submit" value="post">
+         
+</form>
+</div>
+<?php include "../includes/footer.php"; ?>
