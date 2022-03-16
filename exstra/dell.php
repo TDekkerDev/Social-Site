6eb01d87117b724cc447ddc5dedc895df0bce7e4
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-16 09:08:12 +0100

    ua

--- exstra/dell.php
+++ exstra/dell.php
@@ -3,9 +3,13 @@
     $filename = $_POST['imglocation'];
     $id = $_POST['id'];
 
-    $sql = "DELETE FROM bericht WHERE id = :id";
-    $stmt = $db->prepare($sql);
-    $stmt->execute(['id' => $id]);
+    $sql1 = "DELETE FROM comment WHERE post_id = :id";
+    $sth1 = $db->prepare($sql1);
+    $sth1->execute(['id' => $id]);
+
+    $sql2 = "DELETE FROM bericht WHERE id = :id";
+    $stmt2 = $db->prepare($sql2);
+    $stmt2->execute(['id' => $id]);
     unlink("C:/xampp/htdocs/hellowordsource/Social-Site/uplode_media/".$filename);
     header("Location: ../create-bericht/upload_done_change.php");
     exit();
